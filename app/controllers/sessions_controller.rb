c075230875a4f80af51d80487d9fff4266b3b6e4
Tucq <tucq88@gmail.com>
2016-10-30 00:31:12 +0700

    Add user login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,29 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    if @user = User.find_by_email(params[:email])
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:success] = 'Login successfully!'
+        redirect_to root_path
+      else
+        flash[:error] = 'Incorrect password'
+        render 'new'
+      end
+    else
+      flash[:error] = 'User not found with email ' + params[:email]
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Logout successfully!'
+    redirect_to root_path
   end
 
 end

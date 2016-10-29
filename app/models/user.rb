948539e9dc6a5697db4ca63af853dda3f61fce4b
Tucq <tucq88@gmail.com>
2016-10-29 16:52:46 +0000

    Add generated Users

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  has_secure_password
+
+end
